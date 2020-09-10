d44de6e7a3285d4abd551de92468e05ce0ba02d9
Blimbo <dmy.boychuk@gmail.com>
2020-09-10 15:39:08 +0300

    third lesson

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ function getQuantityElements(heightElement) {
 function startGame() {
     start.classList.add('hide');
 
+    gameArea.innerHTML = '';
+
     for (let i = 0; i < getQuantityElements(75); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -48,15 +50,22 @@ function startGame() {
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
-    gameArea.appendChild(car); 
+    gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px'; 
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
+
     requestAnimationFrame(playGame);
 }
 
 function playGame(){
     if(setting.start) {
+        setting.score += setting.speed;
+        score.innerHTML = 'Score<br>' +setting.score;
         moveRoad();
         moveEnemy();
 
@@ -105,6 +114,18 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                setting.start = false;
+                start.classList.remove('hide');
+                start.style.top = score.offsetHeight;
+        } 
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
 
